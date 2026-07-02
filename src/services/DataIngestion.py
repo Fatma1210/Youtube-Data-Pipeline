6ef221b5dfc74bc2bcbab78911cedb6eb614817f
fatma1210 <fatmaibrahim1210@gmail.com>
2026-07-02 17:15:57 +0300

    fix: fix the data ingestion layer to call the mappers correctly with sqlmodels

--- src/services/DataIngestion.py
+++ src/services/DataIngestion.py
@@ -1,9 +1,6 @@
-
-from Enums.YoutubeApiEnums import DataPieceTypeEnum , ResponseKeyEnum
-from mappers import ChannelMapper
-from mappers import VideoMapper
-from mappers import CommentMapper
-
+from mappers.ChannelMapper import ChannelMapper
+from mappers.VideoMapper import VideoMapper
+from mappers.CommentMapper import CommentMapper
 
 
 class DataIngestion:
@@ -11,29 +8,23 @@ class DataIngestion:
         self.api_client = api_client
 
     def ingest_data(self, handle):
-        channel, uploads_playlist_id = self.api_client.get_channel_data(handle)
-        channel_model = ChannelMapper.to_model(channel)
-        print("Channel:", channel_model.title)
-        print("Subscribers:", channel_model.subscriber_count)
-        print("Total videos (reported):", channel_model.video_count)
+        raw_channel, uploads_playlist_id = self.api_client.get_channel_data(handle)
+        if raw_channel is None:
+            return None, [], {}
+
+        channel = ChannelMapper.to_model(raw_channel)
 
         video_ids = self.api_client.get_all_video_ids(uploads_playlist_id)
-        print(f"Found {len(video_ids)} videos")
+        raw_videos = self.api_client.get_videos_data(video_ids)
 
-        videos = self.api_client.get_videos_data(video_ids)
-        video_models = [VideoMapper.to_model(video, channel_model.title, []) for video in videos]
-        for video in video_models[:5]:
-            print(video.title)
-            print(video.published_at, 'published on')
-            print(video.view_count, 'views')
-            print(video.like_count, 'likes')
-            print(video.comment_count, 'comments')
+        videos = []
+        comments_by_video = {}
 
-           
-            comments = self.api_client.get_video_comments(video.id)
-            comment_models = CommentMapper.to_model_list(comments)
-            for comment in comment_models[:3]:
-                print("Comment:",comment.text)
-                print("By:",comment.author)
-                print("Published at:",comment.published_at)
-        return videos
+        for raw_video in raw_videos:
+            video = VideoMapper.to_model(raw_video, channel.id)
+            videos.append(video)
+
+            raw_comments = self.api_client.get_video_comments(video.id)
+            comments_by_video[video.id] = CommentMapper.to_model_list(raw_comments, video.id)
+
+        return channel, videos, comments_by_video
