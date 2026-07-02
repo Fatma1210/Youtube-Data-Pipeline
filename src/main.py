1629e9fbe52c74f342fd407e52dd3d593161f502
fatma1210 <fatmaibrahim1210@gmail.com>
2026-07-02 12:11:15 +0300

    feat: wireup DataTransformation in main.py

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from clients.APIClient import APIClient
 from Enums.YoutubeApiEnums import DataPieceEnum, DataPieceTypeEnum
 from services.DataIngestion import DataIngestion
+from services.DataTransformation import DataTransformation
 from config import Settings
 settings = Settings()
 BASE_URL = settings.BASE_URL
@@ -12,6 +13,7 @@ handles = settings.HANDLES
 client = APIClient(api_key, BASE_URL)
 
 data_ingestion = DataIngestion(client)
+data_transformation = DataTransformation()
 
 
 
@@ -19,4 +21,8 @@ if __name__ == '__main__':
     for handle in handles:
         print(f"\nIngesting data for channel handle: {handle}")
         videos = data_ingestion.ingest_data(handle)
+        transformed_videos = data_transformation.transform_data(videos)
+        print(f"Transformed data for {handle}:")
+        for video in transformed_videos:
+            print(video)
         print(f"Total videos ingested for {handle}: {len(videos)}")
